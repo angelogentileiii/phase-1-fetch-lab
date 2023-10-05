30380d32e634c9af0cb7db32df36bd4242d94122
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-10-05 10:44:18 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function fetchBooks() {
+function fetchBooks(){
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((response) => response.json())
+  .then((data) => renderBooks(data))
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +18,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+fetchBooks();
